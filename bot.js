699fe69af01bfc14e81af6bac5cffd1c73b4f4c0
snowboxx <36881031+snowboxx@users.noreply.github.com>
2018-11-05 17:49:34 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,453 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const ownerid = '369951898268336130'
+client.colors = {}
+const prefix = "*"
+//let rainbow = 0;
+
+ client.on("ready", async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    client.user.setGame(`☆ Store Games ☆`, {type: "2"});
+
+  });
+    client.setInterval(() =>{
+
+        //adding this so it doesnt start doing weird stuff
+        //try to change role color for every server
+        for(let i in client.colors) {
+            let guildId = client.colors[i].guild;
+            let guild = client.guilds.get(guildId);
+            let date = client.colors[i].date;
+
+
+            //if 72 hours have passed, remove from config
+            if(date < new Date().getTime() - 259200000) {
+                 delete client.colors[i];
+                return;
+            }
+
+            //if server gets deleted or bot gets kicked, remove from config
+            if(guild === null) {
+                delete client.colors[i];
+                return;
+            }
+            //try to change the role
+            try{
+                guild.roles.find("name", client.colors[i].role).setColor(rainbow[place])
+                
+                .catch(err => { 
+                    delete client.colors[i]
+                   
+                    return;
+                });
+            }catch(err){
+                delete client.colors[i]
+                return;
+            }
+        }
+        	if(place == (size - 1)) {
+			place = 0;
+		} else {
+			place++;
+		}
+        //Every 10 seconds change it
+    }, 500)
+
+
+client.on('message', message => {
+    var prefix = "*"
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+    if(command === "stats") {
+        var time = process.uptime();
+        var uptime = (time + "").toHHMMSS();
+
+        const embed = new Discord.RichEmbed()
+        .setTitle(":tools: Stats")
+        .setColor(0x009688)
+        .setDescription( 
+        ":crown: " +              "Servers: " + client.guilds.size + "\n" + 
+        ":bust_in_silhouette: " + "Users: " + client.users.size + "\n" + 
+        ":clock12: " +            "Uptime: " + uptime)
+        message.channel.send({embed});
+    }
+    if(command === "rainbow") {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+            const embed = new Discord.RichEmbed()
+            .setAuthor("Rainbow", client.user.avatarURL)
+            .setColor(0xF44336)
+            .setDescription("You must have the administrator permission!")
+            message.channel.send({embed});
+            return;
+        }
+
+        if(!message.guild.me.hasPermission("ADMINISTRATOR")) {
+            const embed = new Discord.RichEmbed()
+            .setAuthor("Rainbow", client.user.avatarURL)
+            .setColor(0xF44336)
+            .setDescription("I must have the administrator permission!")
+            message.channel.send({embed});
+            return;
+        }
+		
+		if(!message.member.guild.roles.find("name", args.join(" "))) {
+            const embed = new Discord.RichEmbed()
+            .setAuthor("Rainbow", client.user.avatarURL)
+            .setColor(0xF44336)
+            .setDescription("Usage: **`r(rainbow (role name)`**")
+            message.channel.send({embed});
+            return;
+        }
+
+        if(message.member.guild.roles.find("name", args.join(" ")) === null) {
+            const embed = new Discord.RichEmbed()
+            .setAuthor("Rainbow", client.user.avatarURL)
+            .setColor(0xF44336)
+            .setDescription("Something went wrong.")
+            message.channel.send({embed});
+            return;
+        }
+
+
+        if(message.member.guild.roles.find("name", args.join(" ")).position >= message.guild.me.highestRole.position) {
+            const embed = new Discord.RichEmbed()
+            .setAuthor("Rainbow", client.user.avatarURL)
+            .setColor(0xF44336)
+            .setDescription("My **RainColor** role must be higher than the mentioned role!")
+            message.channel.send({embed});
+            return;
+        }
+
+
+        const embed = new Discord.RichEmbed()
+        .setAuthor("Rainbow", client.user.avatarURL)
+        .setColor(0x4CAF50)
+        .setDescription("Successfully applied rainbow colors to **`" + args.join(" ") + "`**" + "\n" +
+        "Note: this only lasts 72 hours, then it will stop. You can still apply it whenever you'd like!")
+        message.channel.send({embed});
+
+        client.colors[message.guild.name] = {
+            guild: message.guild.id,
+            role: args.join(" "),
+            date: new Date().getTime()
+        }
+
+        fs.writeFile("./servers.json", JSON.stringify(client.colors, null, 4), err => {
+            if(err) throw err;
+        });
+    }
+});
+
+
+const size    = 12
+const rainbow = new Array(size);
+
+for (var i=0; i<size; i++) {
+	var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
+	var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
+	var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
+	rainbow[i] = '#'+ red + green + blue;
+}
+function sin_to_hex(i, phase) {
+	var sin = Math.sin(Math.PI / size * 2 * i + phase);
+	var int = Math.floor(sin * 127) + 128;
+	var hex = int.toString(16);
+
+	return hex.length === 1 ? '0'+hex : hex;
+}
+let place = 0;
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "439187325503930369") return;
+
+if (message.content.startsWith(prefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
+	 console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+}
+
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+}
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+});
+ client.on('message' , message => {
+  var prefix = "*";
+  if(message.author.bot) return;
+ 
+  if(message.content.startsWith(prefix + "xo")) {
+ let array_of_mentions = message.mentions.users.array();
+  let symbols = [':o:', ':heavy_multiplication_x:']
+  var grid_message;
+ 
+  if (array_of_mentions.length == 1 || array_of_mentions.length == 2) {
+    let random1 = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+    let random2 = Math.abs(random1 - 1);
+    if (array_of_mentions.length == 1) {
+      random1 = 0;
+      random2 = 0;
+    }
+    var player1_id = message.author.id
+    let player2_id = array_of_mentions[random2].id;
+    var turn_id = player1_id;
+    var symbol = symbols[0];
+    let initial_message = `Game match between <@${player1_id}> and <@${player2_id}>!`;
+    if (player1_id == player2_id) {
+      initial_message += '\n_(What a loser, playing this game with yourself :joy:)_'
+    }
+    message.channel.send(`Xo ${initial_message}`)
+    .then(console.log("Successful tictactoe introduction"))
+    .catch(console.error);
+    message.channel.send(':one::two::three:' + '\n' +
+                         ':four::five::six:' + '\n' +
+                         ':seven::eight::nine:')
+    .then((new_message) => {
+      grid_message = new_message;
+    })
+    .then(console.log("Successful tictactoe game initialization"))
+    .catch(console.error);
+    message.channel.send('Loading... Please wait for the :ok: reaction.')
+    .then(async (new_message) => {
+      await new_message.react('1⃣');
+      await new_message.react('2⃣');
+      await new_message.react('3⃣');
+      await new_message.react('4⃣');
+      await new_message.react('5⃣');
+      await new_message.react('6⃣');
+      await new_message.react('7⃣');
+      await new_message.react('8⃣');
+      await new_message.react('9⃣');
+      await new_message.react('🆗');
+      await new_message.edit(`It\'s <@${turn_id}>\'s turn! Your symbol is ${symbol}`)
+      .then((new_new_message) => {
+        require('./xo.js')(client, message, new_new_message, player1_id, player2_id, turn_id, symbol, symbols, grid_message);
+      })
+      .then(console.log("Successful tictactoe listener initialization"))
+      .catch(console.error);
+    })
+    .then(console.log("Successful tictactoe react initialization"))
+    .catch(console.error);
+  }
+  else {
+    message.reply(`use *xo @uesr`)
+    .then(console.log("Successful error reply"))
+    .catch(console.error);
+  }
+}
+ });
+
+client.on('message',async msg => {
+     if(msg.channel.type === "dm") return;
+  if(msg.author.bot) return;
+  var p = "*";
+  if(msg.content.startsWith(p + "setstats")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  var ggg= msg.guild.createChannel('SERVER STATS', 'category').then(cg => {
+           var ccc =msg.guild.createChannel('SERVER STATS', 'voice').then(al => {
+                var aa =msg.guild.createChannel('SERVER STATS', 'voice').then(alp => {
+                   var aaa =msg.guild.createChannel('SERVER STATS', 'voice').then(alph => {
+       al.setParent(cg);
+       alp.setParent(cg);
+       alph.setParent(cg);
+       
+     al.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+     alp.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+     alph.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+hours = currentTime.getHours() + 1 ,
+minutes = currentTime.getMinutes(),
+Seconds = currentTime.getSeconds(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
+     al.setName(`Voice Online :[ ${msg.guild.members.filter(m => m.voiceChannel).size} ]`);
+      alp.setName(`Time :[${hours} : ${minutes} : ${Seconds} ${suffix}]`);
+        alph.setName(` Date : [${Year} - ${Month} - ${Dat} ]`);
+ },1000);
+                   })
+    
+                })
+           })
+  })
+           
+  }
+ 
+});
+client.on('message', async msg => {
+    var p = "."
+  if(msg.content.startsWith(p + "bc")) {
+   let args = msg.content.split(' ').slice(1).join(' ');
+        msg.guild.members.forEach(member => {
+   if(!msg.member.hasPermission('ADMINISTRATOR')) return;
+        member.send(args.replace(`[user]`, member)).catch();
+        })
+ }
+    })
+
+client.on('message' , message => {
+  if(message.content.startsWith("*ping")) {
+ message.channel.send('Pong...').then((msg) => {
+      msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nDiscord API: ${Math.round(client.ping)} ms.\`\`\``);
+ })
+  }  
+ });
+  
+client.on('message', async message => {
+     if(message.author.bot) return;
+if(message.channel.type === "dm") {
+    let embed = new Discord.RichEmbed()
+    .setTimestamp()
+    .setTitle("Direct Message To The Bot")
+    .addField(`Sent By:`,`<@${message.author.id}>`)
+    .setColor("RANDOM")
+    .setThumbnail(message.author.displayAvatarURL)
+    .addField(`Message: `,`\`\`\`${message.content}\`\`\``)
+    .setFooter(`DM Bot Messages | DM Logs`);
+   
+    client.channels.find('id', '490135633143005203').send(embed);
+  }
+});  
+
+client.on('message', message => {
+ if (message.content === "*Rainbow") {
+
+      message.reply('🌈 🌈')
+   
+ let args = message.content.split(' ')
+   
+ args.shift()
+   
+ message.delete()
+   
+ if (!message.member.hasPermission("ADMINISTRATOR"))return console.log("❌ **  Le role `Rainbow` n'existe pas ** ❌") 
+    
+if (!message.guild.roles.find("name", "-Rainbow..")) return message.reply("❌ ** You must make arole whit the name `-Rainbow..`** ❌");
+ 
+   var myRainbow = message.guild.roles.find("name", "-Rainbow..")
+   
+ let i = 0;
+   
+   let interval = setInterval(function () {
+myRainbow.setColor(rainbow[place]).catch(e => {});
+   
+	if(place == (size - 1)) {
+			place = 0;
+		} else {
+			place++;
+		}
+		 
+ }, 1000)
+}
+});
+ client.on ("guildMemberAdd", member => {
+  
+   var role = member.guild.roles.find ("name", "-→Member's ←👤..");
+   member.addRole (role);
+   });
+ client.on('message' , message => {
+    var PREFIX = '*'
+  if (!message.content.startsWith(PREFIX)) return;
+  if (message.author.id !== "439187325503930369") return;
+
+  if(message.content.startsWith(PREFIX + "START")) {
+      var ms = 1000;
+    var nick = [`I`,`IM`,`IM G`,`IM GO`,`IM GOO`,`IM GOOD`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (nick.length) -1 ){
+            j = -1;
+        }
+        i = i+j;
+ message.member.setNickname(nick[i])
+    }, ms);
+  }  
+});    
+const devs = ["439187325503930369"]
+
+const adminprefix = "1";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+    if (message.content === (adminprefix + "Percie")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else     
+    if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}** : Done :>`)
+  return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+  } else
+    if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+        } else     
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+    if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log(`⚠️ Bot restarting... ⚠️`);
+        console.log("===============================================\n\n");
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(`Bot Successfully Restarted`);
+    }
+  
+  });
+client.login(process.env.BOT_TOKEN);
